d32af9e7921f94004f1a468ef136e7639d9a8646
Ziyaad <rhyman.ziyaad@gmail.com>
2022-07-13 19:32:53 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <main>
+    <h1 class="text-3xl font-bold underline text-center">Hello world!</h1>
+  </main>
+  <RouterView />
+</template>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+    apikey: "66b4051b"
+}
