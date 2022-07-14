b5cfe02c9df5e21d2af9ff8fecde5f6f11f5a4eb
Ziyaad <rhyman.ziyaad@gmail.com>
2022-07-14 16:06:51 +0400

    [+][ADD] Fetch movie details

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,11 +1,32 @@
 <template>
     <div>
-        Detail {{ $route.params.id }}
+        <h2>{{ movie.Title }}</h2>
     </div>
 </template>
 
 <script>
-export default {
+import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import env from '@/env.js';
 
+export default {
+    setup() {
+        const movie = ref({});
+        const route = useRoute();
+
+        onBeforeMount(() => {
+            fetch(`${env.baseURL}/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    movie.value = data;
+                });
+        });
+
+        return {
+            movie,
+            route
+        }
+    }
 }
 </script>
